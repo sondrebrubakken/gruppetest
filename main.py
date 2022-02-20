7cac217c807a32f05259824b115e86fd39e452c5
Sondre Brubakken <sondrebrubakken@gmail.com>
2022-02-20 16:38:45 +0100

    test

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from curses import ACS_DARROW
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 
@@ -7,6 +8,9 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return render_template("index.html")
+
+ACS_DARROW
+
 
 if __name__ == "__main__":
     app.run()
